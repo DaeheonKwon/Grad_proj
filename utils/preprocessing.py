16a4a98181a26a19da388b7fa55e884ac02d1423
DaeheonKwon <scientist617@snu.ac.kr>
2023-08-22 15:28:52 +0900

    contamination search ~annotating unique seq in clonotype

---
+++ utils/preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from collections import defaultdict
+
+
+class preprocessing:
+
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.trim_alleles()
+        self.grouped_dataframe = self.dataframe.groupby(['v_call', 'j_call', 'cdr3_aa'])
+
+    def __call__(self):
+        return self.grouped_dataframe
+
+    def trim_alleles(self) -> object:
+        self.dataframe['v_call'] = self.dataframe['v_call'].apply(lambda x: x.split('*')[0])
+        self.dataframe['j_call'] = self.dataframe['j_call'].apply(lambda x: x.split('*')[0])
+        self.dataframe['c_call'] = self.dataframe['cdr3_aa'].apply(lambda x: x.split('*')[0])
